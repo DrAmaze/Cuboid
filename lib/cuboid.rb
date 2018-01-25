fc32c7a0357f6a7bd04e46a1f3edbc10ce3e3cf4
jacob <jbutler8@terpmail.umd.edu>
2018-01-24 16:12:55 -0800

    complete all requirements

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -46,9 +46,58 @@ class Cuboid
     vertices
   end
 
-  #returns true if the two cuboids intersect each other.  False otherwise.
+  # Returns true if the two cuboids intersect each other.
+  # False otherwise. If one cuboid is within another, that counts as
+  # an intersection.
+  # This checks each dimension to ensure that there is at least one
+  # point that overlaps in the (x, y, z) plane.
   def intersects?(other)
+    self_index = dimensions(self.vertices)
+    other_index = dimensions(other.vertices)
+
+    intersection = {}
+    intersection[:x] = self_index[:x] & other_index[:x]
+    intersection[:y] = self_index[:y] & other_index[:y]
+    intersection[:z] = self_index[:z] & other_index[:z]
+
+    intersection.value?([]) ? false : true
   end
 
   #END public methods that should be your starting point
+
+  private
+
+  # Outputs incremented ranges for length, height, and width of a cuboid.
+  # This will be used in calculation to determine if cuboids intersect.
+  # The incrementor used is .01 so as to ensure accuracy, but not
+  # decreases the speed of the program.  For greater accuracy, the
+  # incrementor would be smaller.  For greater speed, the incrementor
+  # would be larger
+  def dimensions(vertices)
+
+    dims = { x: [], y: [], z: [] }
+
+    # length or x-axis
+    if vertices[0][0] < vertices[-1][0]
+      (vertices[0][0]..vertices[-1][0]).step(0.01) { |n| dims[:x] << n }
+    else
+      (vertices[-1][0]..vertices[0][0]).step(0.01) { |n| dims[:x] << n }
+    end
+
+    # height or y-axis
+    if vertices[0][1] < vertices[3][1]
+      (vertices[0][1]..vertices[3][1]).step(0.01) { |n| dims[:y] << n }
+    else
+      (vertices[3][1]..vertices[0][1]).step(0.01) { |n| dims[:y] << n }
+    end
+
+    # width or z-axis
+    if vertices[0][2] < vertices[1][2]
+      (vertices[0][2]..vertices[1][2]).step(0.01) { |n| dims[:z] << n }
+    else
+      (vertices[1][2]..vertices[0][2]).step(0.01) { |n| dims[:z] << n }
+    end
+
+    dims
+  end
 end
